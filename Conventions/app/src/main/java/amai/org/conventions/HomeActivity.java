f10e9000fe6ac2e43998b659c01018e5f4a0cd91
tal-s <tal_sapan@walla.com>
2015-05-01 13:43:05 +0300

    Added a custom toolbar instead of the native one.

--- Conventions/app/src/main/java/amai/org/conventions/HomeActivity.java
+++ Conventions/app/src/main/java/amai/org/conventions/HomeActivity.java
@@ -2,11 +2,13 @@ package amai.org.conventions;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class HomeActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
+
         return true;
     }
 
