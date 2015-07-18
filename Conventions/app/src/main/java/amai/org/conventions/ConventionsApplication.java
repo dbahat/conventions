68cbd2d3e7bb098d17efa0570c2917f5081442ad
David Bahat <dbahat@live.com>
2015-07-18 17:21:31 +0300

    Added google analytics crash and activity reporting

--- Conventions/app/src/main/java/amai/org/conventions/ConventionsApplication.java
+++ Conventions/app/src/main/java/amai/org/conventions/ConventionsApplication.java
@@ -2,11 +2,25 @@ package amai.org.conventions;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 import amai.org.conventions.utils.ConventionStorage;
 
 public class ConventionsApplication extends Application {
+
+	public static GoogleAnalytics analytics;
+	public static Tracker tracker;
+
 	@Override
 	public void onCreate() {
+
 		ConventionStorage.initFromFile(this);
+		analytics = GoogleAnalytics.getInstance(this);
+		analytics.setLocalDispatchPeriod(1800);
+
+		tracker = analytics.newTracker("UA-65293055-1");
+		tracker.enableExceptionReporting(true);
+		tracker.enableAutoActivityTracking(true);
 	}
 }
