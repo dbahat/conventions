92e96c7494f3a8859b082c15de43d789108fee65
David Bahat <dbahat@live.com>
2015-07-24 14:04:48 +0300

    Added special handling for guestOfHonor and games event types, which arrive without any description or images from the server API.

--- Conventions/app/src/main/java/amai/org/conventions/networking/ModelParser.java
+++ Conventions/app/src/main/java/amai/org/conventions/networking/ModelParser.java
@@ -9,10 +9,12 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import amai.org.conventions.R;
 import amai.org.conventions.model.Convention;
 import amai.org.conventions.model.ConventionEvent;
 import amai.org.conventions.utils.Dates;
@@ -63,6 +65,15 @@ public class ModelParser {
                         .withHall(hall)
 		                .withImages(mapper.getImageResourceIds(eventDescription.getEventIds()))
 		                .withId(String.format("%d_%d", eventId, internalEventNumber));
+
+                // Some events (like the guest of honor event and the games event) have special pages and are not retrieved from the API
+                // exposed by the server. For these spacial cases, add special handing of placing hardcoded texts/images.
+                if (conventionEvent.getType() == EventType.GuestOfHonor && conventionEvent.getDescription().length() == 0) {
+                    conventionEvent = handleGuestOfHonorEvent(conventionEvent);
+                }
+                if (conventionEvent.getType() == EventType.Games && conventionEvent.getDescription().length() == 0) {
+                    conventionEvent = handleGamesEvent(conventionEvent);
+                }
 
                 eventList.add(conventionEvent);
                 internalEventNumber++;
@@ -137,5 +148,46 @@ public class ModelParser {
             setEventIds(eventIds);
             return this;
         }
+    }
+
+    private final List<Integer> guestOfHonorImageResourceIds = Arrays.asList( R.drawable.event_reika1,  R.drawable.event_reika2,  R.drawable.event_reika3);
+    private final String guestOfHonorDescription = "<p>השנה, אנחנו שמחים לארח אורחת מיוחדת – רייקה, קוספליירית ידועה שמגיעה אלינו מאוסאקה שביפן.</p>\n" +
+            "<p>רייקה היא קוספליירית יפנית מוכרת, ויש לה כ300 אלף עוקבים <a href=\"https://www.facebook.com/profile.php?id=315573555144954&amp;fref=ts\" target=\"_blank\">בעמוד הפייסבוק שלה</a>.</p>\n" +
+            "<p>היא ידועה בעיקר בזכות הקרוספליי (קוספליי שעושה אדם ממין אחד לדמות מהמין השני) שלה, התחפושות המפורטות ומלאות הפרטים ועבודת האיפור המדוייקת.</p>\n" +
+            "<p>רייקה התחילה להכין קוספליי לראשונה לפני כ18 שנים, ומאז היא לא מפסיקה – ומוזמנת לאינספור כנסים ואירועים מסביב לעולם, ולראשונה גם אלינו לישראל.</p>\n" +
+            "<p>רייקה תקח עמנו השנה חלק בכנס הקרוב במספר פעילויות שיוכרזו בהמשך.</p>";
+
+    private ConventionEvent handleGuestOfHonorEvent(ConventionEvent event) {
+        return event
+                .withDescription(guestOfHonorDescription)
+                .withImages(guestOfHonorImageResourceIds);
+    }
+
+    private final List<Integer> gamesImageResources = Arrays.asList( R.drawable.event_pokemon );
+    private final String gamesEventDescription = "<p><b>טורניר TCG – משחק הקלפים של פוקימון</b></p>\n" +
+            "<p>משחק הקלפים של פוקימון הוא משחק שולחני הידוע בשם TCG. הוא משלב בתוכו אספנות, החלפה ומשחק בקלפים בנושא פוקימון. יש למשחק הזה חוקים משלו, ואלה מבוססים על משחק הוידאו של פוקימון. המשחק הוא מרתק, מהנה ומלא אסטרטגיה. בעת משחק, כל שחקן אוחז ב-60 קלפים, המורכבים מקלפי פוקימון, קלפי מאמן וקלפי אנרגיה, ובאמצעותם עליו לנצח את היריב. דרך הנצחון נקבעת לפי אחת משלושת הדרכים הבאות:</p>\n" +
+            "<ol>\n" +
+            "<li>כאשר שחקן הצליח לאסוף את כל קלפי הפרס שלו.</li>\n" +
+            "<li>כאשר לאחד מהשחקנים אין יותר פוקימונים פעילים בספסל שלו.</li>\n" +
+            "<li>כאשר אחד מהשחקנים לא יכול לשלוף יותר קלפים מהחבילה שלו.</li>\n" +
+            "</ol>\n" +
+            "<p>הפופולריות של המשחק החלה עוד בשנת יציאתו לאור (1996) והיא חזקה כיום יותר מתמיד. לראייה, מומחים מעריכים שנמכרו עד היום כ-15 מיליארד קלפים בסך הכל.</p>\n" +
+            "<p></p>\n" +
+            "<p>טורניר הקלפים יערך לפי החוקים הבאים:</p>\n" +
+            "<ul>\n" +
+            "<li>חוקי נינטנדו המעודכנים.</li>\n" +
+            "<li>לפי רשימת המודיפייד העדכנית.</li>\n" +
+            "<li>טורניר בסגנון סוויס (כולם משחקים מספר שווה של קרבות).</li>\n" +
+            "<li>על כל הקלפים להיות אותנטיים (יש איסור מוחלט להדפיס קלפים).</li>\n" +
+            "<li>על השחקנים להביא את החפיסות מוכנות מהבית.</li>\n" +
+            "</ul>\n" +
+            "<p>&nbsp;</p>\n" +
+            "<p><b>הטורניר בהנחיית יוסי אוחנה</b><b>:</b></p>\n" +
+            "<p>יוסי, בן 31, ידוע ברשת בתור \"היטמן\". מנהל את אתר \"פוקימון: מפלצות כיס\". בוגר תואר ראשון בתקשורת ותרבות מטעם \"מכללת ספיר\", וסטודנט להוראה במכללת \"דוד ילין\". פועל על מנת לקדם את נושא פוקימון בארץ. לקח חלק פעיל בכנסי אמא\"י האחרונים והעביר הרצאות, כגון: \"נוסטלגיה בעולם הפוקימון\" ו\"פוקימון – האם הם באו מן החלל?\"</p>";
+
+    private ConventionEvent handleGamesEvent(ConventionEvent event) {
+        return event
+                .withDescription(gamesEventDescription)
+                .withImages(gamesImageResources);
     }
 }
