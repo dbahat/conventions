daf6d16db0b0493427b8f5d578acfe9995cc75cd
David Bahat <dbahat@live.com>
2015-07-27 20:53:36 +0300

    Extended the google analytics exception parser to include more information

--- Conventions/app/src/main/java/amai/org/conventions/ConventionsApplication.java
+++ Conventions/app/src/main/java/amai/org/conventions/ConventionsApplication.java
@@ -1,11 +1,19 @@
 package amai.org.conventions;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.google.android.gms.analytics.ExceptionReporter;
 import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.StandardExceptionParser;
 import com.google.android.gms.analytics.Tracker;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collection;
+
 import amai.org.conventions.utils.ConventionStorage;
+import amai.org.conventions.utils.Dates;
 
 public class ConventionsApplication extends Application {
 
@@ -22,5 +30,35 @@ public class ConventionsApplication extends Application {
 		tracker = analytics.newTracker("UA-65293055-1");
 		tracker.enableExceptionReporting(true);
 		tracker.enableAutoActivityTracking(true);
+
+		// Change uncaught exception parser to include more information
+		Thread.UncaughtExceptionHandler uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+		if (uncaughtExceptionHandler instanceof ExceptionReporter) {
+			ExceptionReporter exceptionReporter = (ExceptionReporter) uncaughtExceptionHandler;
+			exceptionReporter.setExceptionParser(new ExtendedExceptionParser(this, null));
+		}
+	}
+
+	private static class ExtendedExceptionParser extends StandardExceptionParser {
+
+		public ExtendedExceptionParser(Context context, Collection<String> additionalPackages) {
+			super(context, additionalPackages);
+		}
+
+		@Override
+		public String getDescription(String threadName, Throwable t) {
+			String description = super.getDescription(threadName, t);
+
+			return String.format(Dates.getLocale(), "%s. %s.",
+					description,
+					getFullStackTrace(t));
+		}
+
+		private String getFullStackTrace(Throwable t) {
+			StringWriter stringWriter = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(stringWriter);
+			t.printStackTrace(printWriter);
+			return stringWriter.getBuffer().toString();
+		}
 	}
 }
