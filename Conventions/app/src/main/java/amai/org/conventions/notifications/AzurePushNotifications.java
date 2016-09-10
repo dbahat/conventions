de717ab4fae37ac2e3649d205b9c1184d3b7e9df
dbahat <dbahat@live.com>
2016-09-10 21:27:15 +0300

    Replaced the notification hub + removed un-needed categories

--- Conventions/app/src/main/java/amai/org/conventions/notifications/AzurePushNotifications.java
+++ Conventions/app/src/main/java/amai/org/conventions/notifications/AzurePushNotifications.java
@@ -16,9 +16,9 @@ import amai.org.conventions.utils.CollectionUtils;
 import amai.org.conventions.utils.Log;
 
 public class AzurePushNotifications {
-    public static final String SENDER_ID = "458203975509";
+    public static final String SENDER_ID = "377234495790";
     public static final String HUB_NAME = "conventions";
-    public static final String HUB_LISTEN_CONNECTION_STRING = "Endpoint=sb://conventions.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=hjyET8pIKJ2VZNFkcuvFDt6tZ/aSS57eMSNIU2WofSM=";
+    public static final String HUB_LISTEN_CONNECTION_STRING = "Endpoint=sb://sff-conventions.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=dCrJSDNwpotcGHGwLo4EKObl8cAtVVagaUO3CtPuMSo=";
 
 	private static String TAG = AzurePushNotifications.class.getSimpleName();
 	public static final String IS_ADVANCED_OPTIONS_ENABLED = "isAdvancedOptionsEnabled";
