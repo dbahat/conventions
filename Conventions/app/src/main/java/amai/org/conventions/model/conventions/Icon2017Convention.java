59bdcb27b86af9b9590249f7e33d37b6d4743ef7
tal-s <tal_sapan@walla.com>
2017-10-01 22:27:46 +0300

    Update events cache version

--- Conventions/app/src/main/java/amai/org/conventions/model/conventions/Icon2017Convention.java
+++ Conventions/app/src/main/java/amai/org/conventions/model/conventions/Icon2017Convention.java
@@ -60,7 +60,7 @@ public class Icon2017Convention extends SffConvention {
 
 	@Override
 	protected ConventionStorage initStorage() {
-		return new ConventionStorage(this, R.raw.icon2017_convention_events, 0);
+		return new ConventionStorage(this, R.raw.icon2017_convention_events, 1);
 	}
 
 	@Override
