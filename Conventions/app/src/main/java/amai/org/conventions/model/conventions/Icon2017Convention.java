439723696b00fb61167d36321a0796805beb0600
tal-s <tal_sapan@walla.com>
2017-10-04 11:33:50 +0300

    Update events list

--- Conventions/app/src/main/java/amai/org/conventions/model/conventions/Icon2017Convention.java
+++ Conventions/app/src/main/java/amai/org/conventions/model/conventions/Icon2017Convention.java
@@ -65,7 +65,7 @@ public class Icon2017Convention extends SffConvention {
 
 	@Override
 	protected ConventionStorage initStorage() {
-		return new ConventionStorage(this, R.raw.icon2017_convention_events, 1);
+		return new ConventionStorage(this, R.raw.icon2017_convention_events, 2);
 	}
 
 	@Override
