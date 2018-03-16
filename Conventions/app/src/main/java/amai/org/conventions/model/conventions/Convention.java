87e267184162dfe1063e5a3b3a7f089666f84c4a
tal-s <tal_sapan@walla.com>
2018-03-16 15:42:23 +0200

    Create Olamot2018Convention

--- Conventions/app/src/main/java/amai/org/conventions/model/conventions/Convention.java
+++ Conventions/app/src/main/java/amai/org/conventions/model/conventions/Convention.java
@@ -58,7 +58,7 @@ import sff.org.conventions.BuildConfig;
 
 public abstract class Convention implements Serializable {
 
-	private static Convention convention = new Icon2017Convention();
+	private static Convention convention = new Olamot2018Convention();
 	public static final int NO_COLOR = Color.TRANSPARENT; // Assuming we will never get this from the server...
 
 	// Currently supporting conventions of up to 5 days (UI restriction, since the programme is set
