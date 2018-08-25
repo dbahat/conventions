cc5accf3a2504f95dece963d69618f6a373585a6
tal-s <tal_sapan@walla.com>
2018-08-25 12:55:52 +0300

    Update test date

--- Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
+++ Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
@@ -24,7 +24,7 @@ public class Dates {
 	private static Date getInitialDate() {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm", Dates.getLocale());
 		try {
-			return dateFormat.parse("04.04.2018 17:10");
+			return dateFormat.parse("25.09.2018 17:10");
 		} catch (ParseException e) {
 			throw new RuntimeException(e);
 		}
