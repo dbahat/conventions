68e2dfd8d373796f6321724b4931b019aed4c49f
tal-s <tal_sapan@walla.com>
2019-04-12 22:16:53 +0300

    Refactor second hand activity to "sell" fragment
    Add a tab layout to the second hand activity, currently only showing
    the "sell" tab.

--- Conventions/app/src/main/java/amai/org/conventions/secondhand/SecondHandActivity.java
+++ Conventions/app/src/main/java/amai/org/conventions/secondhand/SecondHandActivity.java
@@ -1,38 +1,26 @@
 package amai.org.conventions.secondhand;
 
-import android.content.DialogInterface;
-import android.database.DataSetObserver;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.ListViewCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.AlertDialog;
-import android.text.InputType;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import amai.org.conventions.ThemeAttributes;
-import amai.org.conventions.model.SecondHand;
-import amai.org.conventions.model.conventions.Convention;
+import java.util.ArrayList;
+import java.util.List;
+
 import amai.org.conventions.navigation.NavigationActivity;
-import amai.org.conventions.utils.Log;
-import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 import sff.org.conventions.R;
 
-public class SecondHandActivity extends NavigationActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class SecondHandActivity extends NavigationActivity {
 	private static final String TAG = SecondHandActivity.class.getCanonicalName();
 
-	private SwipeRefreshLayout swipeRefreshLayout;
-	private View noForms;
-	private StickyListHeadersListView listView;
-	private TextView soldFormsTotal;
-	private SecondHandItemsAdapter adapter;
-	private SecondHand secondHand;
-	private boolean isRefreshing;
-	private boolean isAddingItem;
+	private TabLayout tabLayout;
+	private ViewPager viewPager;
+	private TabAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,173 +28,91 @@ public class SecondHandActivity extends NavigationActivity implements SwipeRefre
 		setContentInContentContainer(R.layout.activity_second_hand);
 		setToolbarTitle(getString(R.string.second_hand));
 
-		secondHand = Convention.getInstance().getSecondHand();
-		swipeRefreshLayout = findViewById(R.id.second_hand_swipe_layout);
-		soldFormsTotal = findViewById(R.id.second_hand_sold_forms_total);
-		noForms = findViewById(R.id.second_hand_no_forms_found);
-		listView = findViewById(R.id.second_hand_form_items_list);
-		adapter = new SecondHandItemsAdapter(secondHand.getForms());
-		listView.setAdapter(adapter);
-		updateSoldForms();
+		tabLayout = findViewById(R.id.second_hand_tabs);
+		viewPager = findViewById(R.id.second_hand_pager);
 
-		isRefreshing = false;
-		isAddingItem = false;
-		swipeRefreshLayout.setOnRefreshListener(this);
-		swipeRefreshLayout.setColorSchemeColors(ThemeAttributes.getColor(this, R.attr.swipeToRefreshColor));
-		swipeRefreshLayout.setProgressBackgroundColorSchemeColor(ThemeAttributes.getColor(this, R.attr.swipeToRefreshBackgroundColor));
-		// This is necessary because for some reason the swipe refresh layout here doesn't recognize that
-		// the sticky headers list view can scroll up, and when scrolling up it always appears which is annoying
-		swipeRefreshLayout.setOnChildScrollUpCallback(new SwipeRefreshLayout.OnChildScrollUpCallback() {
-			@Override
-			public boolean canChildScrollUp(SwipeRefreshLayout parent, @Nullable View child) {
-				return ListViewCompat.canScrollList(listView.getWrappedList(), -1);
-			}
-		});
+		// Setup adapter
+		adapter = new TabAdapter(getSupportFragmentManager());
+		adapter.addFragment(new SecondHandSellFragment(), getString(R.string.second_hand_sell));
 
-		updateListVisibility();
-		adapter.registerDataSetObserver(new DataSetObserver() {
-			@Override
-			public void onChanged() {
-				updateListVisibility();
-				updateSoldForms();
-			}
-		});
-
-		this.setupActionButton(R.drawable.ic_add_white, new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				View dialogView = getLayoutInflater().inflate(R.layout.dialog_edit_text_layout, null, false);
-				final EditText formIdText = dialogView.findViewById(R.id.dialog_edit_text);
-				formIdText.setInputType(InputType.TYPE_CLASS_NUMBER);
-				AlertDialog dialog = new AlertDialog.Builder(SecondHandActivity.this)
-						.setTitle(R.string.add_form)
-						.setMessage(R.string.add_form_instructions)
-						.setView(dialogView)
-						.setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialogInterface, int i) {
-								final String id = formIdText.getText().toString();
-								if (!isInteger(id)) {
-									Toast.makeText(SecondHandActivity.this, R.string.form_id_not_a_number, Toast.LENGTH_LONG).show();
-									return;
-								}
-								isAddingItem = true;
-								updateRefreshing();
-								new AsyncTask<Void, Void, Exception>() {
-									@Override
-									protected Exception doInBackground(Void... params) {
-										try {
-											secondHand.addForm(id);
-											return null;
-										} catch (Exception e) {
-											return e;
-										}
-									}
-
-									@Override
-									protected void onPostExecute(Exception exception) {
-										isAddingItem = false;
-										updateRefreshing();
-										if (exception == null) {
-											adapter.notifyDataSetChanged();
-											listView.post(new Runnable() {
-												@Override
-												public void run() {
-													listView.smoothScrollToPosition(adapter.getLastFormPosition());
-												}
-											});
-										} else {
-											int messageId = R.string.update_refresh_failed;
-											if (exception instanceof SecondHand.FormNotFoundException ||
-													exception instanceof SecondHand.NoItemsException) {
-												messageId = R.string.form_not_found;
-											} else if (exception instanceof SecondHand.FormAlreadyExists) {
-												messageId = R.string.second_hand_form_already_exists;
-											} else {
-												Log.e(TAG, exception.getMessage(), exception);
-											}
-											Toast.makeText(SecondHandActivity.this, messageId, Toast.LENGTH_LONG).show();
-										}
-									}
-								}.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-							}
-						})
-						.setNegativeButton(R.string.cancel, null)
-						.create();
-				dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-					@Override
-					public void onShow(DialogInterface dialogInterface) {
-						formIdText.requestFocus();
-					}
-				});
-				dialog.show();
-			}
-		});
-
-		if (secondHand.shouldAutoRefresh()) {
-			swipeRefreshLayout.post(new Runnable() {
-				@Override
-				public void run() {
-					isRefreshing = true;
-					updateRefreshing();
-					onRefresh();
-				}
-			});
+		if (adapter.getCount() == 1) {
+			tabLayout.setVisibility(View.GONE);
 		}
+
+		// Setup view pager
+		viewPager.setAdapter(adapter);
+		viewPager.setOffscreenPageLimit(adapter.getCount()); // Load all fragments for smooth scrolling
+
+		viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override
+			public void onPageScrolled(int i, float v, int i1) {
+			}
+
+			@Override
+			public void onPageSelected(int i) {
+				triggerFragmentSelected(i);
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int i) {
+			}
+		});
+
+		// Setup tabs
+		tabLayout.setupWithViewPager(viewPager, false);
+
+		viewPager.setCurrentItem(0, false);
+		triggerFragmentSelected(viewPager.getCurrentItem());
 	}
 
-	private boolean isInteger(String string) {
-		try {
-			Integer.valueOf(string);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-
-	private void updateListVisibility() {
-		if (secondHand.getForms().size() == 0) {
-			listView.setVisibility(View.GONE);
-			noForms.setVisibility(View.VISIBLE);
-		} else {
-			listView.setVisibility(View.VISIBLE);
-			noForms.setVisibility(View.GONE);
+	private void triggerFragmentSelected(int i) {
+		Fragment fragment = adapter.getItem(i);
+		if (fragment instanceof OnFragmentSelectedListener) {
+			((OnFragmentSelectedListener) fragment).onFragmentSelected(this);
 		}
 	}
 
 	@Override
-	public void onRefresh() {
-		isRefreshing = true;
-		new AsyncTask<Void, Void, Boolean>() {
-			@Override
-			protected Boolean doInBackground(Void... params) {
-				return secondHand.refresh(true);
-			}
-
-			@Override
-			protected void onPostExecute(Boolean success) {
-				isRefreshing = false;
-				updateRefreshing();
-				adapter.setForms(secondHand.getForms());
-				adapter.notifyDataSetChanged();
-				if (!success) {
-					Toast.makeText(SecondHandActivity.this, R.string.update_refresh_failed, Toast.LENGTH_LONG).show();
-				}
-			}
-		}.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+	public void setupActionButton(int imageResource, View.OnClickListener listener) {
+		super.setupActionButton(imageResource, listener);
 	}
 
-	private void updateSoldForms() {
-		String soldFormsMessage = secondHand.getSoldFormsMessage(SecondHandActivity.this);
-		if (soldFormsMessage != null && !soldFormsMessage.isEmpty()) {
-			soldFormsTotal.setVisibility(View.VISIBLE);
-			soldFormsTotal.setText(soldFormsMessage);
-		} else {
-			soldFormsTotal.setVisibility(View.GONE);
+	@Override
+	protected void removeActionButton() {
+		super.removeActionButton();
+	}
+
+	private class TabAdapter extends FragmentPagerAdapter {
+		private final List<Fragment> fragments = new ArrayList<>();
+		private final List<String> fragmentTitles = new ArrayList<>();
+
+		public TabAdapter(FragmentManager fragmentManager) {
+			super(fragmentManager);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return fragments.get(position);
+		}
+
+		public void addFragment(Fragment fragment, String title) {
+			fragments.add(fragment);
+			fragmentTitles.add(title);
+		}
+
+		@Nullable
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return fragmentTitles.get(position);
+		}
+
+		@Override
+		public int getCount() {
+			return fragments.size();
 		}
 	}
 
-	private void updateRefreshing() {
-		swipeRefreshLayout.setRefreshing(isRefreshing || isAddingItem);
+	public interface OnFragmentSelectedListener {
+		void onFragmentSelected(SecondHandActivity context);
 	}
 }
---
+++ Conventions/app/src/main/java/amai/org/conventions/secondhand/SecondHandSellFragment.java
@@ -0,0 +1,227 @@
+package amai.org.conventions.secondhand;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.DataSetObserver;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.ListViewCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import amai.org.conventions.ThemeAttributes;
+import amai.org.conventions.model.SecondHand;
+import amai.org.conventions.model.conventions.Convention;
+import amai.org.conventions.utils.Log;
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
+import sff.org.conventions.R;
+
+public class SecondHandSellFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, SecondHandActivity.OnFragmentSelectedListener {
+	private static final String TAG = SecondHandSellFragment.class.getCanonicalName();
+
+	private SwipeRefreshLayout swipeRefreshLayout;
+	private View noForms;
+	private StickyListHeadersListView listView;
+	private TextView soldFormsTotal;
+	private SecondHandItemsAdapter adapter;
+	private SecondHand secondHand;
+	private boolean isRefreshing;
+	private boolean isAddingItem;
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		secondHand = Convention.getInstance().getSecondHand();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.fragment_second_hand_sell, container, false);
+
+		swipeRefreshLayout = view.findViewById(R.id.second_hand_sell_swipe_layout);
+		soldFormsTotal = view.findViewById(R.id.second_hand_sold_forms_total);
+		noForms = view.findViewById(R.id.second_hand_no_forms_found);
+		listView = view.findViewById(R.id.second_hand_form_items_list);
+		adapter = new SecondHandItemsAdapter(secondHand.getForms());
+		listView.setAdapter(adapter);
+		updateSoldForms();
+
+		isRefreshing = false;
+		isAddingItem = false;
+		swipeRefreshLayout.setOnRefreshListener(this);
+		Context context = getActivity();
+		swipeRefreshLayout.setColorSchemeColors(ThemeAttributes.getColor(context, R.attr.swipeToRefreshColor));
+		swipeRefreshLayout.setProgressBackgroundColorSchemeColor(ThemeAttributes.getColor(context, R.attr.swipeToRefreshBackgroundColor));
+		// This is necessary because for some reason the swipe refresh layout here doesn't recognize that
+		// the sticky headers list view can scroll up, and when scrolling up it always appears which is annoying
+		swipeRefreshLayout.setOnChildScrollUpCallback(new SwipeRefreshLayout.OnChildScrollUpCallback() {
+			@Override
+			public boolean canChildScrollUp(SwipeRefreshLayout parent, @Nullable View child) {
+				return ListViewCompat.canScrollList(listView.getWrappedList(), -1);
+			}
+		});
+
+		updateListVisibility();
+		adapter.registerDataSetObserver(new DataSetObserver() {
+			@Override
+			public void onChanged() {
+				updateListVisibility();
+				updateSoldForms();
+			}
+		});
+
+		if (secondHand.shouldAutoRefresh()) {
+			swipeRefreshLayout.post(new Runnable() {
+				@Override
+				public void run() {
+					isRefreshing = true;
+					updateRefreshing();
+					onRefresh();
+				}
+			});
+		}
+
+		return view;
+	}
+
+	@Override
+	public void onFragmentSelected(SecondHandActivity context) {
+		context.setupActionButton(R.drawable.ic_add_white, new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				View dialogView = getLayoutInflater().inflate(R.layout.dialog_edit_text_layout, null, false);
+				final EditText formIdText = dialogView.findViewById(R.id.dialog_edit_text);
+				formIdText.setInputType(InputType.TYPE_CLASS_NUMBER);
+				AlertDialog dialog = new AlertDialog.Builder(getActivity())
+						.setTitle(R.string.add_form)
+						.setMessage(R.string.add_form_instructions)
+						.setView(dialogView)
+						.setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialogInterface, int i) {
+								final String id = formIdText.getText().toString();
+								if (!isInteger(id)) {
+									Toast.makeText(getActivity(), R.string.form_id_not_a_number, Toast.LENGTH_LONG).show();
+									return;
+								}
+								isAddingItem = true;
+								updateRefreshing();
+								new AsyncTask<Void, Void, Exception>() {
+									@Override
+									protected Exception doInBackground(Void... params) {
+										try {
+											secondHand.addForm(id);
+											return null;
+										} catch (Exception e) {
+											return e;
+										}
+									}
+
+									@Override
+									protected void onPostExecute(Exception exception) {
+										isAddingItem = false;
+										updateRefreshing();
+										if (exception == null) {
+											adapter.notifyDataSetChanged();
+											listView.post(new Runnable() {
+												@Override
+												public void run() {
+													listView.smoothScrollToPosition(adapter.getLastFormPosition());
+												}
+											});
+										} else {
+											int messageId = R.string.update_refresh_failed;
+											if (exception instanceof SecondHand.FormNotFoundException ||
+													exception instanceof SecondHand.NoItemsException) {
+												messageId = R.string.form_not_found;
+											} else if (exception instanceof SecondHand.FormAlreadyExists) {
+												messageId = R.string.second_hand_form_already_exists;
+											} else {
+												Log.e(TAG, exception.getMessage(), exception);
+											}
+											Toast.makeText(getActivity(), messageId, Toast.LENGTH_LONG).show();
+										}
+									}
+								}.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+							}
+						})
+						.setNegativeButton(R.string.cancel, null)
+						.create();
+				dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+					@Override
+					public void onShow(DialogInterface dialogInterface) {
+						formIdText.requestFocus();
+					}
+				});
+				dialog.show();
+			}
+		});
+	}
+
+	private boolean isInteger(String string) {
+		try {
+			Integer.valueOf(string);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+	private void updateListVisibility() {
+		if (secondHand.getForms().size() == 0) {
+			listView.setVisibility(View.GONE);
+			noForms.setVisibility(View.VISIBLE);
+		} else {
+			listView.setVisibility(View.VISIBLE);
+			noForms.setVisibility(View.GONE);
+		}
+	}
+
+	@Override
+	public void onRefresh() {
+		isRefreshing = true;
+		new AsyncTask<Void, Void, Boolean>() {
+			@Override
+			protected Boolean doInBackground(Void... params) {
+				return secondHand.refresh(true);
+			}
+
+			@Override
+			protected void onPostExecute(Boolean success) {
+				isRefreshing = false;
+				updateRefreshing();
+				adapter.setForms(secondHand.getForms());
+				adapter.notifyDataSetChanged();
+				if (!success) {
+					Toast.makeText(getActivity(), R.string.update_refresh_failed, Toast.LENGTH_LONG).show();
+				}
+			}
+		}.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+	}
+
+	private void updateSoldForms() {
+		String soldFormsMessage = secondHand.getSoldFormsMessage(getContext());
+		if (soldFormsMessage != null && !soldFormsMessage.isEmpty()) {
+			soldFormsTotal.setVisibility(View.VISIBLE);
+			soldFormsTotal.setText(soldFormsMessage);
+		} else {
+			soldFormsTotal.setVisibility(View.GONE);
+		}
+	}
+
+	private void updateRefreshing() {
+		swipeRefreshLayout.setRefreshing(isRefreshing || isAddingItem);
+	}
+}
