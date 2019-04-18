1704a458223bd1d6b34364ce990b221bb43d991c
David Bahat <dbahat@live.com>
2019-04-18 18:40:32 +0300

    Removed un-used activity_discounts.xml

--- Conventions/app/src/main/java/amai/org/conventions/DiscountsActivity.java
+++ Conventions/app/src/main/java/amai/org/conventions/DiscountsActivity.java
@@ -1,12 +1,8 @@
 package amai.org.conventions;
 
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.widget.TextView;
 
 import amai.org.conventions.navigation.NavigationActivity;
-import fi.iki.kuitsi.listtest.ListTagHandler;
 import sff.org.conventions.R;
 
 
@@ -17,11 +13,5 @@ public class DiscountsActivity extends NavigationActivity {
 		int layout = ThemeAttributes.getResourceId(this, R.attr.discountsActivityLayout);
 		setContentInContentContainer(layout);
 		setToolbarTitle(getString(R.string.discounts));
-
-		TextView discountsView = (TextView) findViewById(R.id.discounts_content);
-		if (discountsView != null) {
-			discountsView.setText(Html.fromHtml(getString(R.string.discounts_content), null, new ListTagHandler()));
-			discountsView.setMovementMethod(LinkMovementMethod.getInstance());
-		}
 	}
 }
