b0d4d942ea01c9e59f2ce2d48fd1963d60bb3ae2
tal-s <tal_sapan@walla.com>
2021-08-14 20:48:02 +0300

    Icon 2021 - theme

--- Conventions/app/src/main/java/amai/org/conventions/model/conventions/Icon2021Convention.java
+++ Conventions/app/src/main/java/amai/org/conventions/model/conventions/Icon2021Convention.java
@@ -653,7 +653,7 @@ public class Icon2021Convention extends SffConvention {
 	protected ImageIdToImageResourceMapper initImageMapper() {
 		ImageIdToImageResourceMapper imageMapper = new ImageIdToImageResourceMapper();
 
-		imageMapper.addMapping(ImageIdToImageResourceMapper.EVENT_GENERIC, R.drawable.olamot2021_background);
+		imageMapper.addMapping(ImageIdToImageResourceMapper.EVENT_GENERIC, R.drawable.icon2021_background);
 
 		return imageMapper;
 	}
