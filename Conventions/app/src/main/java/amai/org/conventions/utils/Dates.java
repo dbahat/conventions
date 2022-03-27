19f7afc54aacaa1d7538bc441b11e3598c97319b
tal-s <tal_sapan@walla.com>
2022-03-27 23:15:10 +0300

    Olamot 2021 - theme

--- Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
+++ Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
@@ -27,7 +27,7 @@ public class Dates {
 	private static Date getInitialDate() {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm", Dates.getLocale());
 		try {
-			return dateFormat.parse("22.09.2021 18:10");
+			return dateFormat.parse("19.04.2022 18:10");
 		} catch (ParseException e) {
 			throw new RuntimeException(e);
 		}
