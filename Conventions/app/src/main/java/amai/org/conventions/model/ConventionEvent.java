7fb6544ef673b33e39562b888c014b1e27835a24
tal-s <tal_sapan@walla.com>
2022-04-04 19:19:51 +0300

    Change comment

--- Conventions/app/src/main/java/amai/org/conventions/model/ConventionEvent.java
+++ Conventions/app/src/main/java/amai/org/conventions/model/ConventionEvent.java
@@ -333,7 +333,7 @@ public class ConventionEvent implements Serializable {
 		return this;
 	}
 
-	/** @deprecated Use Convention.getInstance().getEventLocationType(event) */
+	/** @deprecated Use {@link Convention#getEventLocationTypes(ConventionEvent)} */
 	public List<EventLocationType> getLocationTypes() {
 		return locationTypes;
 	}
