079156e90ff1e3c47e28a1b857dd2374e2e5f190
tal-s <tal_sapan@walla.com>
2022-09-12 23:16:16 +0300

    bug fix - FAB flickers when snackbar is opened
    There is still a bug that if the FAB was hidden due to scrolling the app
    bar, it returns when the snackbar is displayed and disappears on further
    scrolls.

--- Conventions/app/src/main/java/amai/org/conventions/FloatingActionButtonScrollBehavior.java
+++ Conventions/app/src/main/java/amai/org/conventions/FloatingActionButtonScrollBehavior.java
@@ -7,13 +7,15 @@ import android.view.View;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import amai.org.conventions.utils.Log;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 /**
  * Scroll the action button off screen when the AppBarLayout is scrolled off screen by the same proportions.
- * Scroll the action button up when the snackbar is displayed (this is done by inheriting FloatingActionButton.Behavior
- * and calling super methods).
+ * Scroll the action button up when the snackbar is displayed (this is done by the CoordinatorLayout automatically because we set the anchor view) and
+ * fix the CoordinatorLayout behavior which flickers the action button.
  */
 public class FloatingActionButtonScrollBehavior extends FloatingActionButton.Behavior {
 	private int toolbarHeight;
@@ -25,7 +27,7 @@ public class FloatingActionButtonScrollBehavior extends FloatingActionButton.Beh
 
 	@Override
 	public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
-		return dependency instanceof AppBarLayout || super.layoutDependsOn(parent, fab, dependency);
+		return dependency instanceof AppBarLayout || dependency instanceof Snackbar.SnackbarLayout || super.layoutDependsOn(parent, fab, dependency);
 	}
 
 	@Override
@@ -49,6 +51,17 @@ public class FloatingActionButtonScrollBehavior extends FloatingActionButton.Beh
 				// Check how much of the app bar layout is off-screen
 				float ratio = dependency.getY() / (float) toolbarHeight;
 				fab.setTranslationY(-distanceToScroll * ratio);
+			}
+			return true;
+		} else if (!isAnchoredToTop && dependency instanceof Snackbar.SnackbarLayout) {
+			// The CoordinatorLayout takes care of changing the action button location according to the snackbar,
+			// but there is a bug where for a single frame the action button is shown above the snackbar while the
+			// snackbar isn't visible (when the animation starts).
+			// Fix this by moving the action button down by the snackbar height, and returning it to its regular location after.
+			if (dependency.getVisibility() != View.VISIBLE) {
+				fab.setTranslationY(dependency.getHeight());
+			} else {
+				fab.setTranslationY(0);
 			}
 			return true;
 		} else if (!isAnchoredToTop) {
