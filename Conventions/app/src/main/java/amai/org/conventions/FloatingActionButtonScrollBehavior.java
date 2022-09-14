f1b617909996ff828b30ccb98bfc240cdaeb39dd
tal-s <tal_sapan@walla.com>
2022-09-14 20:05:18 +0300

    bug fix - FAB flickers when snackbar is opened part 2
    Consider the previous position of the FAB when fixing the position.
    In some cases, the flicker happens when the snackbar is visible, so always
    fix the position the first time.
    This doesn't fix 100% of the cases but almost all of it (sometimes
    the flicker happens the first time after scrolling up and down). In these
    cases it seems the behavior is not called before the wrong position is
    displayed.

--- Conventions/app/src/main/java/amai/org/conventions/FloatingActionButtonScrollBehavior.java
+++ Conventions/app/src/main/java/amai/org/conventions/FloatingActionButtonScrollBehavior.java
@@ -9,8 +9,11 @@ import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import amai.org.conventions.customviews.NestedScrollingFrameLayout;
 import amai.org.conventions.utils.Log;
+import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import sff.org.conventions.R;
 
 /**
  * Scroll the action button off screen when the AppBarLayout is scrolled off screen by the same proportions.
@@ -37,36 +40,61 @@ public class FloatingActionButtonScrollBehavior extends FloatingActionButton.Beh
 			isAnchoredToTop = (((CoordinatorLayout.LayoutParams) fab.getLayoutParams()).anchorGravity & Gravity.TOP) == Gravity.TOP;
 		}
 
-		if (dependency instanceof AppBarLayout) {
-			if (isAnchoredToTop) {
+		if (isAnchoredToTop) {
+			if (dependency instanceof AppBarLayout) {
 				// Move the fab up as much as the app bar layout was changed
 				fab.setTranslationY(dependency.getY());
-			} else {
-				CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
-				int fabBottomMargin = lp.bottomMargin;
-
-				// Check what is the height of the action button (with the bottom margin)
-				int distanceToScroll = fab.getHeight() + fabBottomMargin;
-
-				// Check how much of the app bar layout is off-screen
-				float ratio = dependency.getY() / (float) toolbarHeight;
-				fab.setTranslationY(-distanceToScroll * ratio);
+				return true;
 			}
+			return super.onDependentViewChanged(parent, fab, dependency);
+		}
+
+		// Handle both AppBarLayout and SnackbarLayout
+		if (dependency instanceof AppBarLayout) {
+			CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
+			int fabBottomMargin = lp.bottomMargin;
+
+			// Check what is the height of the action button (with the bottom margin)
+			int distanceToScroll = fab.getHeight() + fabBottomMargin;
+
+			// Check how much of the app bar layout is off-screen
+			float ratio = dependency.getY() / (float) toolbarHeight;
+			fab.setTranslationY(-distanceToScroll * ratio);
+
+			fab.setTag(R.id.fab_translation, fab.getTranslationY());
 			return true;
-		} else if (!isAnchoredToTop && dependency instanceof Snackbar.SnackbarLayout) {
+		} else if (dependency instanceof Snackbar.SnackbarLayout) {
 			// The CoordinatorLayout takes care of changing the action button location according to the snackbar,
 			// but there is a bug where for a single frame the action button is shown above the snackbar while the
 			// snackbar isn't visible (when the animation starts).
 			// Fix this by moving the action button down by the snackbar height, and returning it to its regular location after.
-			if (dependency.getVisibility() != View.VISIBLE) {
-				fab.setTranslationY(dependency.getHeight());
+			float lastTranslationY = 0;
+			if (fab.getTag(R.id.fab_translation) instanceof Float) {
+				lastTranslationY = (float) fab.getTag(R.id.fab_translation);
+			}
+
+			boolean fixedTranslation = false;
+			if (fab.getTag(R.id.fab_fixed_translation) instanceof Boolean) {
+				fixedTranslation = (boolean) fab.getTag(R.id.fab_fixed_translation);
+			}
+
+			if (!fixedTranslation) {
+				fab.setTranslationY(dependency.getHeight() + lastTranslationY);
+				fab.setTag(R.id.fab_fixed_translation, true);
 			} else {
-				fab.setTranslationY(0);
+				fab.setTranslationY(lastTranslationY);
 			}
 			return true;
-		} else if (!isAnchoredToTop) {
-			return super.onDependentViewChanged(parent, fab, dependency);
 		}
-		return false;
+
+		return super.onDependentViewChanged(parent, fab, dependency);
+	}
+
+	@Override
+	public void onDependentViewRemoved(@NonNull CoordinatorLayout parent, @NonNull FloatingActionButton fab, @NonNull View dependency) {
+		if (dependency instanceof Snackbar.SnackbarLayout) {
+			fab.setTag(R.id.fab_fixed_translation, false);
+		}
+		super.onDependentViewRemoved(parent, fab, dependency);
 	}
 }
