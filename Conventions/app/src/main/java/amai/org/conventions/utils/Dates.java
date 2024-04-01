d773562006a9285374b6a1c4ad42ef007c610888
tal-s <tal_sapan@walla.com>
2024-04-01 20:02:39 +0300

    Olamot 2024 - update dummy date

--- Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
+++ Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
@@ -31,7 +31,7 @@ public class Dates {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm", Dates.getLocale());
 		dateFormat.setTimeZone(LOCAL_TIME_ZONE);
 		try {
-//			return dateFormat.parse("03.10.2023 16:10");
+//			return dateFormat.parse("24.04.2024 16:10");
 			return null;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
