79de5576af50b97aa51b1f6da9b03900d2e45205
tal-s <tal_sapan@walla.com>
2025-01-30 19:55:13 +0200

    Allow to use different colors in event feedback when open/closed

--- Conventions/app/src/main/java/amai/org/conventions/events/activities/EventActivity.java
+++ Conventions/app/src/main/java/amai/org/conventions/events/activities/EventActivity.java
@@ -727,16 +727,16 @@ public class EventActivity extends NavigationActivity {
 	private void setupFeedback(ConventionEvent event) {
 		if (event.canFillFeedback()) {
 			feedbackContainer.setVisibility(View.VISIBLE);
-			updateFeedbackState(event);
 
-			feedbackView.setAdditionalFeedbackURL(Convention.getInstance().getAdditionalEventFeedbackURL(event));
-			feedbackView.setModel(event.getUserInput().getFeedback());
-
+			// This should be called before setModel for the color states to be correct
 			if (shouldFeedbackBeClosed()) {
 				feedbackView.setState(CollapsibleFeedbackView.State.Collapsed, false);
 			} else {
 				feedbackView.setState(CollapsibleFeedbackView.State.Expanded, false);
 			}
+
+			feedbackView.setAdditionalFeedbackURL(Convention.getInstance().getAdditionalEventFeedbackURL(event));
+			feedbackView.setModel(event.getUserInput().getFeedback());
 
 			feedbackView.setSendFeedbackClickListener(feedbackView.new CollapsibleFeedbackViewSendListener() {
 				@Override
@@ -756,6 +756,12 @@ public class EventActivity extends NavigationActivity {
 					updateFeedbackState(event);
 				}
 			});
+
+			feedbackView.setOnStateChangedListener(() -> {
+				updateFeedbackState(event);
+			});
+
+			updateFeedbackState(event);
 		} else {
 			feedbackContainer.setVisibility(View.GONE);
 		}
@@ -769,6 +775,10 @@ public class EventActivity extends NavigationActivity {
 		}
 		if (!Convention.getInstance().isFeedbackSendingTimeOver()) {
 			states.add(R.attr.state_event_feedback_can_send);
+		}
+
+		if (feedbackView.getState() == CollapsibleFeedbackView.State.Collapsed) {
+			states.add(R.attr.state_feedback_collapsed);
 		}
 		states.setForView(feedbackContainer);
 	}
