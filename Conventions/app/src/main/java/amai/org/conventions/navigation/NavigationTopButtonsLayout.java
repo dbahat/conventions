f86ca4f54bcac25b6b48be958e687253e7f50142
tal-s <tal_sapan@walla.com>
2025-09-06 11:58:15 +0300

    add separate attribute for navigation top row selected background

--- Conventions/app/src/main/java/amai/org/conventions/navigation/NavigationTopButtonsLayout.java
+++ Conventions/app/src/main/java/amai/org/conventions/navigation/NavigationTopButtonsLayout.java
@@ -10,50 +10,50 @@ import android.widget.LinearLayout;
 
 import java.util.List;
 
-import sff.org.conventions.R;
+import amai.org.conventions.R;
 import amai.org.conventions.ThemeAttributes;
 import amai.org.conventions.customviews.AspectRatioImageView;
 import androidx.annotation.Nullable;
 
 public class NavigationTopButtonsLayout extends LinearLayout {
 
-	public NavigationTopButtonsLayout(Context context, @Nullable AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public NavigationTopButtonsLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public void setNavigationItems(final NavigationActivity currentActivity, List<NavigationItem> navigationItems) {
-		removeAllViews();
+    public void setNavigationItems(final NavigationActivity currentActivity, List<NavigationItem> navigationItems) {
+        removeAllViews();
 
-		for (final NavigationItem item : navigationItems) {
-			AspectRatioImageView imageView = new AspectRatioImageView(getContext());
-			boolean isSelected = currentActivity.getClass() == item.getActivity();
-			Drawable icon = item.getIcon();
-			if (isSelected && item.getSelectedIcon() != null) {
-				icon = item.getSelectedIcon();
-			}
-			int color;
-			if (isSelected) {
-				color = ThemeAttributes.getColor(currentActivity, R.attr.navigationPopupTopSelectedColor);
-                int backgroundColor = ThemeAttributes.getColor(currentActivity, R.attr.navigationPopupSelectedBackground);
+        for (final NavigationItem item : navigationItems) {
+            AspectRatioImageView imageView = new AspectRatioImageView(getContext());
+            boolean isSelected = currentActivity.getClass() == item.getActivity();
+            Drawable icon = item.getIcon();
+            if (isSelected && item.getSelectedIcon() != null) {
+                icon = item.getSelectedIcon();
+            }
+            int color;
+            if (isSelected) {
+                color = ThemeAttributes.getColor(currentActivity, R.attr.navigationPopupTopSelectedColor);
+                int backgroundColor = ThemeAttributes.getColor(currentActivity, R.attr.navigationPopupTopSelectedBackground);
                 imageView.setBackgroundColor(backgroundColor);
-			} else {
-				color = ThemeAttributes.getColor(currentActivity, R.attr.navigationPopupTopNotSelectedColor);
+            } else {
+                color = ThemeAttributes.getColor(currentActivity, R.attr.navigationPopupTopNotSelectedColor);
                 imageView.setBackgroundColor(Color.TRANSPARENT);
-			}
-			icon.mutate().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-			imageView.setImageDrawable(icon);
+            }
+            icon.mutate().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+            imageView.setImageDrawable(icon);
 
-			int width = getResources().getDimensionPixelSize(R.dimen.navigation_settings_button_width);
-			imageView.setLayoutParams(new LinearLayout.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT));
-			int padding = getResources().getDimensionPixelSize(R.dimen.navigation_settings_button_padding);
-			imageView.setPadding(padding, padding, padding, padding);
-			addView(imageView);
+            int width = getResources().getDimensionPixelSize(R.dimen.navigation_settings_button_width);
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT));
+            int padding = getResources().getDimensionPixelSize(R.dimen.navigation_settings_button_padding);
+            imageView.setPadding(padding, padding, padding, padding);
+            addView(imageView);
 
-			if (isSelected) {
-				imageView.setOnClickListener(null);
-			} else {
-				imageView.setOnClickListener(view -> currentActivity.navigateToActivity(item.getActivity()));
-			}
-		}
-	}
+            if (isSelected) {
+                imageView.setOnClickListener(null);
+            } else {
+                imageView.setOnClickListener(view -> currentActivity.navigateToActivity(item.getActivity()));
+            }
+        }
+    }
 }
