95020f5867faa69dbc8b902e60e026bd27f7af53
tal-s <tal_sapan@walla.com>
2026-01-24 15:21:59 +0200

    Harucon 2026 - update settings convention ID, change debug date

--- Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
+++ Conventions/app/src/main/java/amai/org/conventions/utils/Dates.java
@@ -34,7 +34,7 @@ public class Dates {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm", Dates.getLocale());
 		dateFormat.setTimeZone(LOCAL_TIME_ZONE);
 		try {
-//			return dateFormat.parse("28.08.2025 14:50");
+//			return dateFormat.parse("03.03.2026 14:50");
 			return null;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
