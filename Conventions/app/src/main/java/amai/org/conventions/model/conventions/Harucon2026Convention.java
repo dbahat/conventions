80c42a367120824d8b3c485913f62fc5a070cb07
tal-s <tal_sapan@walla.com>
2026-02-17 19:45:10 +0200

    Harucon 2026 - update events cache

--- Conventions/app/src/main/java/amai/org/conventions/model/conventions/Harucon2026Convention.java
+++ Conventions/app/src/main/java/amai/org/conventions/model/conventions/Harucon2026Convention.java
@@ -115,7 +115,7 @@ public class Harucon2026Convention extends AmaiConvention {
 
 	@Override
 	protected ConventionStorage initStorage() {
-		return new ConventionStorage(this, R.raw.harucon2026_convention_events, 0);
+		return new ConventionStorage(this, R.raw.harucon2026_convention_events, 1);
 	}
 
 	@Override
