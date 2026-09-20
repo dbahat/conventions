22c7a9bb78993faaa023cd7bf0652be42552e89a
tal-s <tal_sapan@walla.com>
2026-09-20 19:34:02 +0300

    Icon 2026 - add discounts, sponsors and activities

--- Conventions/app/src/main/java/amai/org/conventions/navigation/NavigationActivity.java
+++ Conventions/app/src/main/java/amai/org/conventions/navigation/NavigationActivity.java
@@ -36,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 
 import amai.org.conventions.AboutActivity;
+import amai.org.conventions.ActivitiesActivity;
 import amai.org.conventions.ApplicationInitializer;
 import amai.org.conventions.ArrivalMethodsActivity;
 import amai.org.conventions.ConventionsApplication;
@@ -345,8 +346,8 @@ public abstract class NavigationActivity extends AppCompatActivity {
 		if (Convention.getInstance().canFillFeedback()) {
 			items.add(new NavigationItem(FeedbackActivity.class, getString(R.string.feedback), ContextCompat.getDrawable(this, R.drawable.feedback_menu_icon), shouldDisplayIcon));
 		}
-//		items.add(new NavigationItem(DiscountsActivity.class, getString(R.string.discounts), ContextCompat.getDrawable(this, R.drawable.ic_card_giftcard_white), shouldDisplayIcon));
-//		items.add(new NavigationItem(ActivitiesActivity.class, getString(R.string.activities), ContextCompat.getDrawable(this, R.drawable.ic_card_giftcard_white), shouldDisplayIcon));
+		items.add(new NavigationItem(DiscountsActivity.class, getString(R.string.discounts), ContextCompat.getDrawable(this, R.drawable.ic_card_giftcard_white), shouldDisplayIcon));
+		items.add(new NavigationItem(ActivitiesActivity.class, getString(R.string.activities), ContextCompat.getDrawable(this, R.drawable.ic_card_giftcard_white), shouldDisplayIcon));
 		items.add(new NavigationItem(WebContentActivity.AccessibilityActivity.class, getString(R.string.accessibility), ContextCompat.getDrawable(this, R.drawable.baseline_accessibility_new_white_18), null, true, true));
 		items.add(new NavigationItem(AboutActivity.class, getString(R.string.about), ContextCompat.getDrawable(this, R.drawable.ic_action_about), shouldDisplayIcon));
 //		items.add(new NavigationItem(SettingsActivity.class, getString(R.string.settings), ContextCompat.getDrawable(this, R.drawable.ic_settings), shouldDisplayIcon));
